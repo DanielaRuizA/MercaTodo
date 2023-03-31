2ca8b2a4a1e989f90c9bbf2a81c322238d00800c
Daniela Ruiz <daruiz1218@gmail.com>
2023-03-30 19:25:40 -0500

    fix bad commit

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -9,9 +9,6 @@ use App\Http\Requests\UserRequest;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         return Inertia::render('Admin/Users/Index', [
@@ -19,45 +16,16 @@ class UserController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(UserRequest $request, User $user)
-    {
-        $user->create($request->all());
-
-        $user = User::create($request->all());
-
-        return redirect()->route('user.edit', $user->id)->with('message', 'Nota creada');
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(User $user)
     {
         return Inertia::render('Admin/Users/Show', compact('user'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(User $user)
     {
         return Inertia::render('Admin/Users/Edit', compact('user'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(UserRequest $request, User $user)
     {
         $user->update($request->all());
@@ -65,9 +33,6 @@ class UserController extends Controller
         return redirect()->route('users.index')->with('message', 'Usuario Actualizado');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(User $user)
     {
         $user->delete();
