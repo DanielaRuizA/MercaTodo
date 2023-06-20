45e68d806443d55abd9614a3b3e4dc6d2ffe2d74
Daniela Ruiz <daruiz1218@gmail.com>
2023-06-20 11:03:35 -0500

    refactor: status users

--- resources/js/Pages/AdminPanel/Users/Index.vue
+++ resources/js/Pages/AdminPanel/Users/Index.vue
@@ -31,7 +31,14 @@ export default {
     },
     methods: {
         updateStatus(user) {
-            const status = user.status ? 0 : 1;
+            // const status = user.status ? 0 : 1;
+            // const status = user.status === 'Inactive' ? 'Inactive' : 'Active';
+            // const status = user.status ? 'Active' : 'Inactive';
+            // const status = user.status === 'Active' ? 'Active' : 'Inactive';
+            // const status = user.status ? 'Active' : 'Inactive';
+            // const status = user.status ? 'Inactive' : 'Active';
+            // const status = user.status === 'Inactive' ? 'Inactive' : 'Active';
+            const status = user.status ? 'Inactive' : 'Active';
             axios.get('/change/status', {
                 params: { status: status, user_id: user.id }
             }).then(response => {
@@ -40,6 +47,17 @@ export default {
             }).catch(error => {
                 console.error(error);
             });
+            // updateStatus(user) {
+            //     const status = user.status ? 'Inactive' : 'Active';
+            //     axios.get('/change/status', {
+            //         params: { status: status, user_id: user.id }
+            //     }).then(response => {
+            //         console.log(response.data.success);
+            //         user.status = !user.status;
+            //     }).catch(error => {
+            //         console.error('Error al cambiar el estado:', error);
+            //     });
+            // }
         }
     }
 }
@@ -102,9 +120,12 @@ export default {
                     <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
                         {{ user.email }}
                     </td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                        {{ user.status }}
+                    </td>
                     <td>
                         <label class="relative inline-flex items-center mr-5 cursor-pointer">
-                            <input type="checkbox" value="" class="sr-only peer" :checked="user.status"
+                            <input type="checkbox" value="" class="sr-only peer" :checked="user.status === 'Inactive'"
                                 @change="updateStatus(user)" :data-id="user.id">
                             <div
                                 class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600">
