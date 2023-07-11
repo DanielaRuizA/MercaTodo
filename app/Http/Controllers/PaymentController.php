096feb74d7497a2f512940bf98f2a386989362d6
Daniela Ruiz <daruiz1218@gmail.com>
2023-07-10 21:36:53 -0500

    fix: payment controller

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -1,20 +1,26 @@
 <?php
 
+
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
 use App\Models\Order;
 use Inertia\Response;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Services\PaymentBase;
+use Illuminate\Validation\Rule;
 use App\Services\PlaceToPayPayment;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
 use App\Domain\Order\OrderCreateAction;
+use App\Domain\Order\OrderUpdateAction;
+use Illuminate\Support\Facades\Redirect;
 use Gloudemans\Shoppingcart\Facades\Cart;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Response as HttpResponse;
 
 class PaymentController extends Controller
 {
-    public function processPayment(Request $request, Order $order, PlaceToPayPayment $paymentService): HttpResponse
+    public function processPayment(Request $request, Order $order, PlaceToPayPayment $paymentService)
     {
         $order = OrderCreateAction::execute($request->all());
 
@@ -45,7 +51,7 @@ class PaymentController extends Controller
         Cart::instance('default')->destroy();
     }
 
-    public function processResponse(PlaceToPayPayment $placeToPayPayment): Response
+    public function processResponse(PlaceToPayPayment $placeToPayPayment)
     {
         // dd($placeToPayPayment->getRequestInformation());
 
@@ -53,7 +59,7 @@ class PaymentController extends Controller
     }
     
 
-    public function retryPayment(Request $request, PlaceToPayPayment $paymentService):RedirectResponse
+    public function retryPayment(Request $request, PlaceToPayPayment $paymentService)
     {
         $request->validate([
             'id' => ['required', 'integer'],
